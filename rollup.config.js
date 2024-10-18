c0ebc310eddf3ee2c1d906d167c533fd327d2ac8
ngo275 <ngo38114@gmail.com>
2024-10-18 09:39:38 +0700

    Init repo

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'es',
+      sourcemap: true,
+    }
+  ],
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.json',
+    }),
+    terser() // Minifies the output bundle
+  ],
+  external: []
+};
+
